8b2142692ab5f98ab13afe1bf22e7ee5b8a38c76
Melycode <melissagc21@gmail.com>
2025-11-03 09:21:32 +0100

    Initial commit

---
+++ src/cocheMejorado.java
@@ -0,0 +1,103 @@
+public class cocheMejorado {
+    private String marca;
+    private String modelo;
+    private int anyoFabricacion;
+    private double combustibleActual;
+    private double consumo;
+    private double kilometraje;
+
+
+    public cocheMejorado(String marca, String modelo, int anyoFabricacion, double combustibleActual, double consumo, double kilometraje) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.anyoFabricacion = anyoFabricacion;
+        this.combustibleActual = combustibleActual;
+        this.consumo = consumo;
+        this.kilometraje = kilometraje;
+    }
+
+    public cocheMejorado() {
+    }
+
+    public cocheMejorado(String marca, String modelo, int anyoFabricacion) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.anyoFabricacion = anyoFabricacion;
+        this.combustibleActual = 0.0;
+        this.consumo = 0.0;
+        this.kilometraje = 0.0;
+    }
+
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getAnyoFabricacion() {
+        return anyoFabricacion;
+    }
+
+    public void setAnyoFabricacion(int anyoFabricacion) {
+        this.anyoFabricacion = anyoFabricacion;
+    }
+
+    public double getCombustibleActual() {
+        return combustibleActual;
+    }
+
+    public void setCombustibleActual(double combustibleActual) {
+        this.combustibleActual = combustibleActual;
+    }
+
+    public double getConsumo() {
+        return consumo;
+    }
+
+    public void setConsumo(double consumo) {
+        this.consumo = consumo;
+    }
+
+    public double getKilometraje() {
+        return kilometraje;
+    }
+
+    public void setKilometraje(double kilometraje) {
+        this.kilometraje = kilometraje;
+    }
+
+
+    public void conducir(double km){
+        this.kilometraje = this.kilometraje + km;
+        this.combustibleActual = this.combustibleActual - (km * this.consumo / 100);
+
+    }
+
+
+    public void repostar(double litros){
+        this.combustibleActual += litros;
+    }
+
+
+    public double getAutonomia(){
+        return (this.combustibleActual / this.consumo) * 100;
+}
+
+    @Override
+    public String toString() {
+        return this.marca + this.modelo;
+    }
+    
+}
+
