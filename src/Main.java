b6c98ed4c1281e50e63a8fe66670e42c942ddadf
Melycode <melissagc21@gmail.com>
2025-11-03 10:32:04 +0100

    improve +

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-
-
-
-
+    cocheMejorado cm = new cocheMejorado("Opel", "Astra", 2011);
+        System.out.println(cm);
+        System.out.println(cm.getCombustibleActual());
+        cm.repostar(200);
+        cm.setConsumo(10);
+        cm.conducir(6);
+        System.out.println(cm.getCombustibleActual());
+        System.out.println(cm.getKilometraje());
     }
 }
